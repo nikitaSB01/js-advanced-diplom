8e5993aa72cff440c66d04cc5db9a1da20156135
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-03-03 15:55:39 +0600

    сделал генератор и Team

--- src/js/generators.js
+++ src/js/generators.js
@@ -1,3 +1,4 @@
+import Team from './Team';
 /**
  * Формирует экземпляр персонажа из массива allowedTypes со
  * случайным уровнем от 1 до maxLevel
@@ -10,6 +11,13 @@
  */
 export function* characterGenerator(allowedTypes, maxLevel) {
   // TODO: write logic here
+  while (true) {
+    const randomLevel = Math.ceil(Math.random() * maxLevel);
+    const randomIndex = Math.floor(Math.random() * allowedTypes.length);
+    const char = new allowedTypes[randomIndex]();
+    char.level = randomLevel;
+    yield char;
+  }
 }
 
 /**
@@ -17,8 +25,16 @@ export function* characterGenerator(allowedTypes, maxLevel) {
  * @param allowedTypes массив классов
  * @param maxLevel максимальный возможный уровень персонажа
  * @param characterCount количество персонажей, которое нужно сформировать
- * @returns экземпляр Team, хранящий экземпляры персонажей. Количество персонажей в команде - characterCount
+ * @returns экземпляр Team, хранящий экземпляры персонажей. Количество персонажей
+ * в команде - characterCount
  * */
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
   // TODO: write logic here
+  const generTeam = characterGenerator(allowedTypes, maxLevel);
+  const team = [];
+  for (let i = 0; i < characterCount; i += 1) {
+    const generChar = generTeam.next().value;
+    team.push(generChar);
+  }
+  return new Team(team);
 }
