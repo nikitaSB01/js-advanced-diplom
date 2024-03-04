0417116339ac31b2dae033ad687d88c841a5db3b
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-03-04 12:04:07 +0600

    finalized the gameContr.test

--- src/__tests__/gameController.test.js
+++ src/__tests__/gameController.test.js
@@ -3,7 +3,8 @@ import GamePlay from '../js/GamePlay';
 import Vampire from '../js/characters/Vampire';
 
 test('Create tooltip message', () => {
-  const gameContr = new GameController(GamePlay);
+  const gamePlay = new GamePlay();
+  const gameContr = new GameController(gamePlay);
   const vampir = new Vampire(1);
   const res = gameContr.createMessage(vampir);
   const expected = '\u{1F396} 1 \u{2694} 25 \u{1F6E1} 25 \u{2764} 50';
