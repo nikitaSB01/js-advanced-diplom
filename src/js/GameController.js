b2a1a68e8ea3b558c94f144be93d3f16d42e3732
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-03-04 11:10:15 +0600

    прописал логику onCellEnter и немного onCellLeave

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,14 +1,14 @@
-import GamePlay from './GamePlay';
-import themes from './themes';
-import PositionedCharacter from './PositionedCharacter';
-import Bowman from './characters/Bowman';
-import Swordsman from './characters/Swordsman';
-import Magician from './characters/Magician';
-import Vampire from './characters/Vampire';
-import Daemon from './characters/Daemon';
-import Undead from './characters/Undead';
-import GameState from './GameState';
-import { generateTeam } from './generators';
+import GamePlay from "./GamePlay";
+import themes from "./themes";
+import PositionedCharacter from "./PositionedCharacter";
+import Bowman from "./characters/Bowman";
+import Swordsman from "./characters/Swordsman";
+import Magician from "./characters/Magician";
+import Vampire from "./characters/Vampire";
+import Daemon from "./characters/Daemon";
+import Undead from "./characters/Undead";
+import GameState from "./GameState";
+import { generateTeam } from "./generators";
 
 export default class GameController {
   constructor(gamePlay, stateService) {
@@ -16,11 +16,11 @@ export default class GameController {
     this.stateService = stateService;
     this.fieldSize = this.gamePlay.boardSize;
 
-  /*  this.onCellClick = this.onCellClick.bind(this);
+    this.onCellClick = this.onCellClick.bind(this);
     this.onCellEnter = this.onCellEnter.bind(this);
     this.onCellLeave = this.onCellLeave.bind(this);
 
-    this.addEvents(); */
+    this.addEvents();
   }
 
   init() {
@@ -28,13 +28,23 @@ export default class GameController {
     this.level = 1;
 
     this.gamePlay.drawUi(this.theme);
-    this.playerTeam = generateTeam([Bowman, Swordsman, Magician], this.level, 3);
-    this.playerPositions = this.generatePositions('playerTeam');
-    this.positionedPlayerTeam = this.createPositionedTeam(this.playerTeam, this.playerPositions);
+    this.playerTeam = generateTeam(
+      [Bowman, Swordsman, Magician],
+      this.level,
+      3
+    );
+    this.playerPositions = this.generatePositions("playerTeam");
+    this.positionedPlayerTeam = this.createPositionedTeam(
+      this.playerTeam,
+      this.playerPositions
+    );
 
     this.enemyTeam = generateTeam([Vampire, Undead, Daemon], this.level, 3);
-    this.enemyPositions = this.generatePositions('enemyTeam');
-    this.positionedEnemyTeam = this.createPositionedTeam(this.enemyTeam, this.enemyPositions);
+    this.enemyPositions = this.generatePositions("enemyTeam");
+    this.positionedEnemyTeam = this.createPositionedTeam(
+      this.enemyTeam,
+      this.enemyPositions
+    );
     this.allChars = [...this.positionedPlayerTeam, ...this.positionedEnemyTeam];
 
     this.gamePlay.redrawPositions(this.allChars);
@@ -44,9 +54,9 @@ export default class GameController {
     const positions = [];
     for (let i = 0; i < this.fieldSize; i += 1) {
       for (let j = 0; j < this.fieldSize; j += 1) {
-        if (string === 'playerTeam' && j < 2) {
+        if (string === "playerTeam" && j < 2) {
           positions.push(i * this.fieldSize + j);
-        } else if (string === 'enemyTeam' && j >= this.fieldSize - 2) {
+        } else if (string === "enemyTeam" && j >= this.fieldSize - 2) {
           positions.push(i * this.fieldSize + j);
         }
       }
@@ -68,15 +78,41 @@ export default class GameController {
     return positionedTeam;
   }
 
+  addEvents() {
+    this.gamePlay.addCellEnterListener(this.onCellEnter);
+    this.gamePlay.addCellClickListener(this.onCellClick);
+    this.gamePlay.addCellLeaveListener(this.onCellLeave);
+
+    this.gamePlay.addNewGameListener(() => this.newGame());
+    this.gamePlay.addSaveGameListener(() => this.saveGame());
+    this.gamePlay.addLoadGameListener(() => this.loadGame());
+  }
+
   onCellClick(index) {
     // TODO: react to click
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  createMessage(char) {
+    return `\u{1F396} ${char.level} \u{2694} ${char.attack} \u{1F6E1} ${char.defence} \u{2764} ${char.health}`;
+  }
+
   onCellEnter(index) {
     // TODO: react to mouse enter
+    const cellWithChar = this.gamePlay.cells[index].querySelector(".character");
+    this.enteredCell = this.gamePlay.cells[index];
+
+    // Проверяем, есть ли персонаж в наведенной клетке
+    if (cellWithChar) {
+      this.enteredChar = this.allChars.find((char) => char.position === index);
+      const mess = this.createMessage(this.enteredChar.character);
+      this.gamePlay.showCellTooltip(mess, index);
+      this.gamePlay.setCursor('pointer');
+    }
   }
 
   onCellLeave(index) {
     // TODO: react to mouse leave
+    this.gamePlay.hideCellTooltip(index);
   }
 }
